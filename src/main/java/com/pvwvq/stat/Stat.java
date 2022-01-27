1d00919e9c58256ba0d0b48bec5d23a730e59ebd
2-sun <qwerkevin1103@gmail.com>
2022-01-27 17:50:52 +0900

    Initial commit

---
+++ src/main/java/com/pvwvq/stat/Stat.java
@@ -0,0 +1,17 @@
+package com.pvwvq.stat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Stat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
